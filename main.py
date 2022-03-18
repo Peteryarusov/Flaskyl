489979bf9ed9964d7c1f1b0220f8b66ec969505b
petr7 <petr.78ya@gmail.com>
2022-03-18 11:51:08 +0300

    test commit5

--- main.py
+++ main.py
@@ -7,7 +7,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Привет Андрееей"
+    return """
+    <html>
+    <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Пример веб-страницы</title>
+    </head>
+    <body>
+    <h1>Заголовок</h1>
+    <!-- Комментарий -->
+    <p>Первый абзац.</p>
+    <p>Второй абзац.</p>
+    </body>
+    </html>"""
 
 
 if __name__ == '__main__':
